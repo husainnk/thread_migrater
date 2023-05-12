db59c45a35a024600316194a8c5034c0ea5cf7df
nkhusain <nkhusain@localhost>
2023-05-11 20:01:01 -0600

    support thread selection

--- migrater.py
+++ migrater.py
@@ -14,10 +14,12 @@ import json
 
 CRIT_PATH ="/users/nkhusain/TransProc/criu-3.15/crit/crit"
 img =  "pstree.img"
-TARGET_THREAD = 1
+TARGET_THREAD = int(sys.argv[1])
+
+print("Target child thread: ",TARGET_THREAD)
+
 
 show_cmd = "python3 "+ CRIT_PATH + " show " + img ;
-print(show_cmd)
 
 show_out  = subprocess.run(show_cmd.split(' '),stdout=subprocess.PIPE)
 show_out = show_out.stdout.decode("utf-8")
@@ -26,27 +28,30 @@ pstree_object = json.loads(show_out)
 
 threads_list = pstree_object['entries'][0]['threads']
 
+print("available threads\n");
+print(threads_list);
+
 new_list  = []
 new_list.append(threads_list[0])
 
+# Copy only the main thread ID
 pstree_object['entries'][0]['threads'] = new_list	
 print(pstree_object)
+
 
 json_data = json.dumps(pstree_object, indent=4)
 f = open('pstree.json', 'w')
 f.write(json_data)
 f.close()
 
+# Process core images
 dump_cmd = "python3 "+ CRIT_PATH + " encode -i pstree.json -o " + img ;
 dump_out = subprocess.run(dump_cmd.split(' '),stdout=subprocess.PIPE)
 dump_out = dump_out.stdout.decode("utf-8")
 
-print(dump_out)
-
-
+#print(dump_out)
 
 PID=threads_list[0]
-
 
 show_cmd = "python3 "+ CRIT_PATH + " show " + "core-" + str(PID) +".img" ;
 
@@ -54,11 +59,10 @@ show_out  = subprocess.run(show_cmd.split(' '),stdout=subprocess.PIPE)
 show_out = show_out.stdout.decode("utf-8")
 
 core_main_object = json.loads(show_out)
+tc_object = core_main_object['entries'][0]['tc']; # TC values from main thread
 
-tc_object = core_main_object['entries'][0]['tc'];
-
-PID=threads_list[1]
-show_cmd = "python3 "+ CRIT_PATH + " show " + "core-" + str(PID) +".img" ;
+TID=threads_list[TARGET_THREAD]
+show_cmd = "python3 "+ CRIT_PATH + " show " + "core-" + str(TID) +".img" ;
 show_out  = subprocess.run(show_cmd.split(' '),stdout=subprocess.PIPE)
 show_out = show_out.stdout.decode("utf-8")
 
